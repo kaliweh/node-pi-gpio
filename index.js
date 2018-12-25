281648cc48c6d68c0936be4da8c7a79bf6ccd617
khalidaliweh <khalidaliweh@gmail.com>
2018-12-24 23:38:18 -0500

    fixing async issue

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const blobService = storage.createBlobService();
 const Gpio = require('onoff').Gpio; // Gpio class
 const pir = new Gpio(17, 'in', 'both');    // Export GPIO17 as both
 
-
-pir.watch(function (err, value) {
+pir.watch(async(err, value)=> {
     if (value == 1) {
         let imgName = '';
         let img = '';
         for(let i=0; i<5;i++){
-            imgName = `${__dirname}/img/cap-${Date.now()}.jpeg`;
+            imgName = `cap-${Date.now()}.jpeg`;
             img = await cam.takeImage();
             blobService.createBlockBlobFromText('sink-images',imgName,img);
             console.log('took image', i);

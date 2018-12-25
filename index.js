45649099230cf64481c5b700dc686f7b4b97ff9d
Ubuntu <kaliweh@sink-virtual-machine.wamyha2tbi4e3jcdydtpf1hhad.cx.internal.cloudapp.net>
2018-12-25 03:50:35 +0000

    installing dependencies

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const Camera = require('pi-camera');
-const myCam = new Camera({mode:'photo',output:`${__dirname}/img.jpg`, noPreview:true});
+const StillCamera = require('pi-camera-connect').StillCamera;
+const cam = new StillCamera();
 const Gpio = require('onoff').Gpio; // Gpio class
 const pir = new Gpio(17, 'in', 'both');    // Export GPIO17 as both
  
-// Toggle the state of the LED connected to GPIO17 every 200ms.
-// Here synchronous methods are used. Asynchronous methods are also available.
-const iv = setInterval(() => led.writeSync(led.readSync() ^ 1), 200);
  
 pir.watch(function(err, value) {
     if (value == 1) {
+
+
         console.log('sensor 1');
     } else {
         sendMessage('sensor 0');

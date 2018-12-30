a374fc9b65b67afb276e9fdecfd977a96a11b773
khalidaliweh <khalidaliweh@gmail.com>
2018-12-30 12:37:00 -0500

    changing the implementaion

--- index.js
+++ index.js
@@ -5,27 +5,34 @@ const blobService = storage.createBlobService();
 const Gpio = require('onoff').Gpio; // Gpio class
 const pir = new Gpio(17, 'in', 'both');    // Export GPIO17 as both
 
-pir.watch(async (err, value) => {
+const uploadImage = (img, imageName)=>{
+    blobService.createBlockBlobFromText('sink-images', imgName, img, function (error, result, response) {
+        if (error != null) {
+            console.log('connection error... image will be lost!:', error);
+        }
+        else {
+            console.log('uploaded image', result.name);
+            return response;
+        }
+    });
+};
+
+pir.watch((err, value) => {
     if (value == 1) {
         let imgName = '';
-        let img = '';
         for (let i = 0; i < 3; i++) {
             imgName = `cap-${Date.now()}.jpeg`;
-            try {
-                img = await cam.takeImage();
-            } catch (err) {
-                console.log('error occured while taking an image.', err);
-                continue;
-            }
-            blobService.createBlockBlobFromText('sink-images', imgName, img, function (error, result, response) {
-                if (error != null) {
-                    console.log('connection error... image will be lost!:', error);
-                }
-                else {
-                    console.log('uploaded image', result.name);
-                    return response;
-                }
-            });
+                cam.takeImage().then((img)=>{
+                    uploadImage(img, imgName);
+                }).catch(err=>{console.log('error occured while taking an image.', err);})
+                    
+                        
+                
+            // } catch (err) {
+            //     console.log('error occured while taking an image.', err);
+            //     continue;
+            // }
+          
         }
     } else {
         console.log('pir is 0');

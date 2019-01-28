26ab9ec4395ec1517575e2fefb8da48e0e8809d8
khalidaliweh <khalidaliweh@gmail.com>
2019-01-27 20:26:00 -0500

    Sending the events to the storage queue

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const StillCamera = require('pi-camera-connect').StillCamera;
 const cam = new StillCamera({ width: 800, height: 600 });
 const storage = require('azure-storage');
 const blobService = storage.createBlobService();
+const queueService = storage.createQueueService();
 const Gpio = require('onoff').Gpio; // Gpio class
 const pir = new Gpio(17, 'in', 'both');    // Export GPIO17 as both
 const MAX_EVENT_CAPTURE_COUNT = 2;
 
-console.log('['+new Date().toLocaleString() + '] starting to listen to sink events.....'); 
+console.log('[' + new Date().toLocaleString() + '] starting to listen to sink events.....');
 
 const uploadImage = (img, imgName) => {
-
     blobService.createBlockBlobFromText('sink-images', imgName, img, function (error, result, response) {
         if (error != null) {
             console.log('connection error... image will be lost!:', error);
         }
         else {
-            console.log('['+ new Date().toLocaleString() + '] '+result.name);
+            console.log('[' + new Date().toLocaleString() + '] ' + result.name);
             count++
+            queueService.createMessage('sink-event-queue', imgName, function (QueueError) {
+                if (QueueError != null) {
+                    console.log('Error occured while sending event queue message, this means the event will not be proccesd! event [' + imgName + ']', QueueError);
+                }
+            });
             captureEventImage();
             return response;
         }
@@ -26,13 +31,13 @@ const uploadImage = (img, imgName) => {
 let count = 0;
 
 const captureEventImage = () => {
-   // console.log('captureEventImage called with count ' + count);
+    // console.log('captureEventImage called with count ' + count);
     if (count > MAX_EVENT_CAPTURE_COUNT) {
         return;
     }
     let imgName = `cap-${Date.now()}`;
     cam.takeImage().then((img) => {
-   //     console.log('I am starting to upload an image');
+        //     console.log('I am starting to upload an image');
         uploadImage(img, imgName);
     }).catch(err => { console.log('error occured while taking an image. ' + err); })
 
